33e5a8afe596ec827be191246c6219ebb0ca3541
Femidev1 <128675924+Femidev1@users.noreply.github.com>
2024-09-15 19:03:49 -0400

    first commit

---
+++ src/main.js
@@ -0,0 +1,28 @@
+import Phaser from "phaser";
+import Bootloader from "./scenes/bootloader";
+import Outro from "./scenes/outro";
+import Splash from "./scenes/splash";
+import Transition from "./scenes/transition";
+import Game from "./scenes/game";
+
+const config = {
+  width: 360,
+  height: 640,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
+  autoRound: false,
+  parent: "contenedor",
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 0 },
+      debug: false,
+    },
+  },
+  scene: [Bootloader, Splash, Transition, Game, Outro],
+};
+
+
+const game = new Phaser.Game(config);
